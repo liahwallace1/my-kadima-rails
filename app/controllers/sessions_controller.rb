529b3f94ecdb0934c79820b7af1d8ad9ebb48a1f
Liah Wallace <liahwallace1@gmail.com>
2017-03-28 16:48:02 +0000

    updated sessions controller, layout partials, and user show page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(params[:user][:username])
+    @user = User.find_by(username: params[:user][:username])
     @user = @user.try(:authenticate, params[:user][:password])
     return redirect_to('/login') unless @user
     session[:user_id] = @user.id
