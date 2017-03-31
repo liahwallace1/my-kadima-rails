6c9ba9fb62ba88f1c8f9b6e302f2163d9cf2f800
Liah Wallace <liahwallace1@gmail.com>
2017-03-31 19:48:36 +0000

    added custom validation to game date_played attribute

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,7 +3,13 @@ class Game < ApplicationRecord
   has_many :players, through: :game_players, :foreign_key => "user_id"
   belongs_to :location
 
-  validates_presence_of :distance, :game_type, :volley_total
+  validates_presence_of :date_played, :distance, :game_type, :volley_total
+  validate :date_played_cannot_be_in_the_future
+
+  def date_played_cannot_be_in_the_future
+   if date_played.present? && date_played > Date.today
+     errors.add(:date_played, "can't be in the future")
+   end
 
   def played_with=(new_players)
     self.players = new_players.split(',').map do |name|
