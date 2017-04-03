a16ffc2977bce86f42c9a8afd20b126e138b640c
Liah Wallace <liahwallace1@gmail.com>
2017-04-03 14:47:41 +0000

    updating played_with method and form field

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,20 +6,28 @@ class Game < ApplicationRecord
   validates_presence_of :date_played, :distance, :game_type, :volley_total
   validate :date_played_cannot_be_in_the_future
 
+  before_save :user_is_player
+
   def date_played_cannot_be_in_the_future
    if date_played.present? && date_played > Date.today
      errors.add(:date_played, "can't be in the future")
    end
  end
 
-  def played_with=(new_players)
-    self.players=new_players.split(',').map do |name|
-      User.find_by(username: username.strip)
+  def played_with=(player_list)
+    self.players = player_list.split(', ').map do |player_name|
+      User.find_by(username: player_name.strip)
     end.compact
   end
 
   def played_with
-    self.players.map{|p| p.username }.join.(', ')
+    players.map{|p| p.username }.join.(', ') if save
+  end
+
+  def user_is_player
+    if user && !players.include?(user)
+      players << user
+    end
   end
 
 end
