1f7e1870929508a8f37cbc08638860b68f17e949
Liah Wallace <liahwallace1@gmail.com>
2017-04-04 18:10:06 +0000

    added method in game controller to make sure game creator was game player

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,4 +1,5 @@
 class GamesController < ApplicationController
+  before_action :require_login
 
   def index
     if params[:user_id]
@@ -10,19 +11,17 @@ class GamesController < ApplicationController
 
   def show
     set_game
-    if !logged_in?
-      redirect_to root_path
-    end
   end
 
   def new
   end
 
   def create
-    @game = Game.new(game_params)
+    @game = Game.create(game_params)
     @game.date_played = Date.new(params[:game]["date_played(1i)"].to_i, params[:game]["date_played(2i)"].to_i, params[:game]["date_played(3i)"].to_i)
     if @game.save
       @game.played_with = params[:game][:played_with]
+      creator_is_player
       @game.save
       redirect_to game_path(@game)
     else
@@ -43,8 +42,18 @@ class GamesController < ApplicationController
 
   private
 
+  def require_login
+    redirect_to root_path unless session.include? :user_id
+  end
+
   def set_game
     @game ||= Game.find_by_id(params[:id])
+  end
+
+  def creator_is_player
+    if current_user && !@game.players.include?(current_user)
+      @game.players << current_user
+    end
   end
 
   def game_params
--- app/models/game.rb
+++ app/models/game.rb
@@ -5,14 +5,30 @@ class Game < ApplicationRecord
 
   validates_presence_of :date_played, :distance, :game_type, :volley_total
   validate :date_played_cannot_be_in_the_future
+  validate :two_types_of_games
+  validate :three_distance_options
 
-  # before_save :creator_is_player
 
+## VALIDATION METHODS ##
   def date_played_cannot_be_in_the_future
    if date_played.present? && date_played > Date.today
      errors.add(:date_played, "can't be in the future")
    end
  end
+
+ def two_types_of_games
+   if game_type != 'one-on-one' && game_type != 'multi-player'
+     errors.add(:game_type, "must be 'one-on-one' OR 'multi-player'")
+   end
+ end
+
+ def three_distance_options
+   if distance != 'short' && distance != 'medium' && distance != 'long'
+     errors.add(:distance, "must be 'short', 'medium', OR 'long'")
+   end
+ end
+
+## CUSTOM SETTER FOR PLAYERS ##
 
   def played_with=(player_list)
     player_list.split(', ').map do |player_name|
@@ -25,10 +41,7 @@ class Game < ApplicationRecord
     players.map{|p| p.username }.join.(', ') if save
   end
 
-  # def creator_is_player
-  #   if current_user && !players.include?(current_user)
-  #     players << user
-  #   end
-  # end
+
+  ## SCOPE METHODS FOR STATS ##
 
 end
