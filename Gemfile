10af4bd767f6b55894051b6ac12698576af087bc
Liah Wallace <liahwallace1@gmail.com>
2017-04-05 20:57:47 +0000

    added figaro and configured omniauth for facebook

--- Gemfile
+++ Gemfile
@@ -5,6 +5,12 @@ git_source(:github) do |repo_name|
   "https://github.com/#{repo_name}.git"
 end
 
+#access OAuth protocols
+gem 'omniauth'
+#access OAuth for Facebook
+gem 'omniauth-facebook'
+#figaro to store Omniauth provider keys
+gem 'figaro'
 #bcrypt for secure password
 gem 'bcrypt'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170327170911) do
+ActiveRecord::Schema.define(version: 20170405204722) do
 
   create_table "game_players", force: :cascade do |t|
     t.integer  "game_id"
@@ -46,6 +46,8 @@ ActiveRecord::Schema.define(version: 20170327170911) do
     t.boolean  "public_view",     default: false
     t.datetime "created_at",                      null: false
     t.datetime "updated_at",                      null: false
+    t.string   "provider"
+    t.string   "uid"
   end
 
 end
