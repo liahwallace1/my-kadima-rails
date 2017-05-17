0f94a9760adccfd315afc115d2b28f95ad8a339e
liahwallace1 <liahwallace1@gmail.com>
2017-05-17 15:48:23 -0400

    added to bindClickHandlers, created formatIndex() to add table row for each game in Index page

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -5,7 +5,16 @@ $(() => {
 const bindClickHandlers = () => {
   $(".see-games").on("click", (e) => {
     e.preventDefault();
-    console.log("hello")
+    fetch(`/users/id/posts.json`)
+    .then((res) => res.json())
+    .then(data => {
+      $('#main-content').html("")
+      data.forEach((game) => {
+        let newGame = new Game(game)
+        let gameHTML = newGame.formatIndex()
+        $('#main-content').append(gameHTML)
+      })
+    })
   })
 }
 
@@ -19,5 +28,20 @@ function Game(game) {
   this.distance = game.distance
   this.game_type = game.game_type
   this.volley_total = game.volley_total
-  this.location_id = game.location_id
+  this.location = game.location
+  this.played_with = game.played_with
+}
+
+Game.prototype.formatIndex = () => {
+  let gameHTML = `
+    <tr class="clickable-row" data-href="<%= game_path(game) %>">
+      <td>${this.date_played}</td>
+      <td>${this.location.name}</td>
+      <td>${this.game_type}</td>
+      <td>${this.distance}</td>
+      <td>${this.volley_total}</td>
+      <td>${this.played_with}</td>
+    </tr>
+  `
+  return gameHTML
 }
