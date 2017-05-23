7b501e4a21843b8e311306565a62bdb3ae0e8502
liahwallace1 <liahwallace1@gmail.com>
2017-05-23 16:01:19 -0400

    added new user serializer method to deal with no-game situation with user profile stats

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -11,12 +11,12 @@ const bindUserClickHandlers = () => {
     clearContent();
     getProfile(userId);
   });
-  $(document).on("click", "button.edit-user", () => {
-    let userId = $("button.edit-user").data("userid");
-    history.pushState(null, null, `/users/${userId}/edit`);
-    clearContent();
-    getEditUser(userId);
-  });
+  // $(document).on("click", "button.edit-user", () => {
+  //   let userId = $("button.edit-user").data("userid");
+  //   history.pushState(null, null, `/users/${userId}/edit`);
+  //   clearContent();
+  //   getEditUser(userId);
+  // });
 }
 
 
@@ -27,10 +27,12 @@ function User(user) {
   this.username = user.username
   this.games = user.games
   this.locations = user.locations
-  this.high_score_single_score = user.high_score_single_score
-  this.high_score_single_partner = user.high_score_single_partner
-  this.high_score_group_score = user.high_score_group_score
-  this.high_score_group_partners = user.high_score_group_partners
+  // this.high_score_single_score = user.high_score_single_score
+  // this.high_score_single_partner = user.high_score_single_partner
+  // this.high_score_group_score = user.high_score_group_score
+  // this.high_score_group_partners = user.high_score_group_partners
+  this.one_on_one_stat = user.one_on_one_stat
+  this.group_stat = user.group_stat
   this.frequent_partner_name = user.frequent_partner_name
   this.frequent_location_name = user.frequent_location_name
   this.number_of_games = user.number_of_games
@@ -44,9 +46,9 @@ User.prototype.formatProfile = function() {
   <button class="btn btn-primary add-game" data-userid="${this.id}">Add a New Game</button>
   <h3>Your Stats:</h3>
 
-  <p id="high-score-single"></p><br>
+  <p id="high-score-single"><strong>High Score for One-on-One Game: </strong>${this.one_on_one_stat}</p><br>
 
-  <p id="high-score-multi"></p><br>
+  <p id="high-score-multi"><strong>High Score for Group Game: </strong>${this.group_stat}</p><br>
 
   <p><strong>Number of Games: </strong>${this.number_of_games}</p><br>
 
@@ -61,25 +63,25 @@ User.prototype.formatProfile = function() {
   return profileHTML
 }
 
-User.prototype.formatHighGameSingle = function() {
-  let text = ""
-  if (this.high_score_single_score) {
-    text = `<strong>High Score for One-on-One Game: </strong>${this.high_score_single_score} volleys with Player ${this.high_score_single_partner}`
-  } else {
-    text = `<strong>High Score for One-on-One Game: </strong>No one-on-one games yet!`
-  }
-  return $('#high-score-single').html(text)
-}
-
-User.prototype.formatHighGameMulti = function() {
-  let text = ""
-  if (this.high_score_group_score) {
-    text = `<strong>High Score for Group Game: </strong>${this.high_score_group_score} volleys with Players ${this.high_score_group_partners}`
-  } else {
-    text = `<strong>High Score for Group Game: </strong>No multi-player games yet!`
-  }
-  return $('#high-score-multi').html(text)
-}
+// User.prototype.formatHighGameSingle = function() {
+//   let text = ""
+//   if (this.high_score_single_score) {
+//     text = `<strong>High Score for One-on-One Game: </strong>${this.high_score_single_score} volleys with Player ${this.high_score_single_partner}`
+//   } else {
+//     text = `<strong>High Score for One-on-One Game: </strong>No one-on-one games yet!`
+//   }
+//   return $('#high-score-single').html(text)
+// }
+//
+// User.prototype.formatHighGameMulti = function() {
+//   let text = ""
+//   if (this.high_score_group_score) {
+//     text = `<strong>High Score for Group Game: </strong>${this.high_score_group_score} volleys with Players ${this.high_score_group_partners}`
+//   } else {
+//     text = `<strong>High Score for Group Game: </strong>No multi-player games yet!`
+//   }
+//   return $('#high-score-multi').html(text)
+// }
 
 const getProfile = (userId) => {
   $.ajax({
